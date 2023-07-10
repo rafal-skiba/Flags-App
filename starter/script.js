20fb598cb5347f5acb393b1e88d4a53833b8f282
Rafał Skiba <rr.skiba92@gmail.com>
2023-07-10 21:43:55 +0200

    initial commit

---
+++ starter/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.com/v3.1/name/poland');
+
+request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+  console.log(data.flags);
+  const html = `  
+<article class="country">
+<img class="country__img" src="${data.flags.png}" />
+<div class="country__data">
+  <h3 class="country__name">${data.name.common}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+    1
+  )} people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages.pol}</p>
+  <p class="country__row"><span>💰</span>${data.currencies.PLN.name}</p>
+</div>
+</article>
+`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
