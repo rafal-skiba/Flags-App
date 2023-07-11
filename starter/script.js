ee81e8740fadfc6eb85d9217dfa30def7ade6213
Rafał Skiba <rr.skiba92@gmail.com>
2023-07-11 23:38:41 +0200

    new request to API added

--- starter/script.js
+++ starter/script.js
@@ -5,31 +5,50 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
-
-request.open('GET', 'https://restcountries.com/v3.1/name/poland');
-
-request.send();
-
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-  console.log(data.flags);
+const renderCountry = function (data, className = '') {
   const html = `  
-<article class="country">
-<img class="country__img" src="${data.flags.png}" />
-<div class="country__data">
-  <h3 class="country__name">${data.name.common}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
-    1
-  )} people</p>
-  <p class="country__row"><span>🗣️</span>${data.languages.pol}</p>
-  <p class="country__row"><span>💰</span>${data.currencies.PLN.name}</p>
-</div>
-</article>
-`;
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages}</p>
+    <p class="country__row"><span>💰</span>${data.currencies}</p>
+  </div>
+  </article>
+  `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
-});
+};
+
+const getCountryDataAndBorder = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    renderCountry(data);
+
+    //Get neighbour country
+
+    const neighbour = data.borders?.[0];
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryDataAndBorder('Poland');
